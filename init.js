9c8af13d3d147e7766660e90a8ce1d40cc4c4e78
Celso Milleo <celsomilleo@hotmail.com>
2022-05-20 19:42:25 -0300

    feat-gameAudio

--- init.js
+++ init.js
@@ -1,60 +1,56 @@
-const body = document.getElementsByTagName('body')[0];
+const body = document.getElementsByTagName("body")[0];
 
-function initPage(){
+function initPage() {
+  const main = document.createElement("main");
+  main.id = "mainInit";
+  const backGround = document.createElement("img");
+  backGround.src = "./img/bg-1.png";
+  const container = document.createElement("div");
+  container.classList.add("containerInit");
+  const cardTop = document.createElement("div");
+  cardTop.id = "cardTop";
+  const cardTopNav = document.createElement("span");
+  cardTopNav.classList.add("material-icons-sharp");
+  cardTopNav.innerText = "rocket_launch";
+  const cardTopTitle = document.createElement("h1");
+  cardTopTitle.innerText = "Rocket Genius";
+  const cardMain = document.createElement("div");
+  cardMain.id = "cardMain";
+  const cardMainUl = document.createElement("ul");
+  const cardMainLi1 = document.createElement("li");
+  const namePlayer = document.createElement("h2");
+  namePlayer.innerText = "Nome do Jogador";
+  const inputPlayer = document.createElement("input");
+  inputPlayer.type = "text";
+  inputPlayer.placeholder = "Digite seu nome";
+  inputPlayer.id = "inputPlayer";
+  const buttonPlay = document.createElement("button");
+  buttonPlay.id = "buttonPlay";
+  buttonPlay.innerText = "Launch";
 
-    const main = document.createElement('main');
-    main.id = 'mainInit';
-    const backGround = document.createElement('img');
-    backGround.src = './img/bg-1.png';
-    const container = document.createElement('div');
-    container.classList.add('containerInit');
-    const cardTop = document.createElement('div');
-    cardTop.id = 'cardTop';
-    const cardTopNav = document.createElement('span');
-    cardTopNav.classList.add("material-icons-sharp");
-    cardTopNav.innerText = 'rocket_launch'
-    const cardTopTitle = document.createElement('h1');
-    cardTopTitle.innerText = 'Rocket Genius';
-    const cardMain = document.createElement('div');
-    cardMain.id = 'cardMain';
-    const cardMainUl = document.createElement('ul');
-    const cardMainLi1 = document.createElement('li');
-    const namePlayer = document.createElement('h2');
-    namePlayer.innerText = 'Nome do Piloto';
-    const inputPlayer = document.createElement('input');
-    inputPlayer.type = 'text';
-    inputPlayer.placeholder = 'Digite seu nome';
-    inputPlayer.id = 'inputPlayer';
-    const buttonPlay = document.createElement('button');
-    buttonPlay.id = 'buttonPlay';
-    buttonPlay.innerText = 'Launch';
-    
-
-    
-    
-    main.appendChild(container);
-    container.appendChild(cardTop);
-    cardTop.appendChild(cardTopNav);
-    cardTop.appendChild(cardTopTitle);
-    cardMain.appendChild(cardMainUl);
-    cardMainUl.appendChild(cardMainLi1);
-    cardMainLi1.appendChild(namePlayer);
-    cardMainLi1.appendChild(inputPlayer);
-    cardMainLi1.appendChild(buttonPlay);
-    container.appendChild(cardMain);
-    body.appendChild(main);
-    body.appendChild(backGround);
+  main.appendChild(container);
+  container.appendChild(cardTop);
+  cardTop.appendChild(cardTopNav);
+  cardTop.appendChild(cardTopTitle);
+  cardMain.appendChild(cardMainUl);
+  cardMainUl.appendChild(cardMainLi1);
+  cardMainLi1.appendChild(namePlayer);
+  cardMainLi1.appendChild(inputPlayer);
+  cardMainLi1.appendChild(buttonPlay);
+  container.appendChild(cardMain);
+  body.appendChild(main);
+  body.appendChild(backGround);
 }
 initPage();
-const namePlayer = document.getElementById('inputPlayer').value;
-const buttonPlay = document.getElementById('buttonPlay');
-buttonPlay.addEventListener('click', ()=>{
-  const init = document.getElementById('init');
-  init.href = 'style.css';
-  
-  body.innerHTML = '';
-  const script = document.createElement('script');
-  script.src = 'script.js';
-  body.appendChild(script);
-
-})
+const inputPlayer = document.getElementById("inputPlayer");
+const buttonPlay = document.getElementById("buttonPlay");
+buttonPlay.addEventListener("click", () => {
+  const init = document.getElementById("init");
+  init.href = "style.css";
+  const main = document.getElementById("mainInit");
+  const img = document.getElementsByTagName("img")[0];
+  main.remove();
+  img.remove();
+  const namePlayer = inputPlayer.value;
+  initGame(namePlayer);
+});
--- script.js
+++ script.js
@@ -1,80 +1,180 @@
-initGame(namePlayer);
+const red = { color: "red", audio: new Audio("./audio/audio-1.mp3") },
+  blue = { color: "blue", audio: new Audio("./audio/audio-2.mp3") },
+  yellow = { color: "yellow", audio: new Audio("./audio/audio-3.mp3") },
+  green = { color: "green", audio: new Audio("./audio/audio-4.mp3") };
 
-function initGame(namePlayer){
-    const main = document.createElement('main');
-    main.innerHTML = 
-        `
-            <div class="container">
-                <div class="bol">
-                <button id="play">Play</button></div>
-                <div class="green"></div>
-                <div class="red"></div>
-                <div class="yellow"></div>
-                <div class="blue"></div>
-            </div>
-            
-        `
-    main.id = 'mainGame';
-    body.appendChild(main);
-    /* let player = new Player(namePlayer);
-    let computer = new Computer();
-    let game = new Game(player, computer);
-    game.startGame(); */
+function initGame(namePlayer) {
+  const body = document.body;
+
+  const section = document.createElement("section");
+  section.id = "caixa";
+  body.appendChild(section);
+  const colors = document.createElement("div");
+  colors.id = "colors";
+  const divGreen = document.createElement("div");
+  divGreen.className = "green";
+
+  const divRed = document.createElement("div");
+  divRed.className = "red";
+
+  const divYellow = document.createElement("div");
+  divYellow.className = "yellow";
+
+  const divBlue = document.createElement("div");
+  divBlue.className = "blue";
+
+  const console = document.createElement("div");
+  console.className = "console";
+  const nameGame = document.createElement("h1");
+  nameGame.innerText = "Rocket Genius";
+  const namePlayerText = document.createElement("h2");
+  namePlayerText.innerText = `${namePlayer}`;
+  const level = document.createElement("h3");
+  level.innerText = "Clique nas cores conforme sequência na tela";
+  level.id = "levelH3";
+  const pcMessages = document.createElement("h3");
+  pcMessages.id = "pcMessages";
+  const buttonReset = document.createElement("button");
+  buttonReset.id = "buttonReset";
+  buttonReset.innerText = "Iniciar";
+  const imagem = document.createElement("img");
+  imagem.className = "nave";
+  imagem.src = "./img/bg-2.png";
+  console.append(nameGame, namePlayerText, level, pcMessages, buttonReset);
+  colors.append(console, divGreen, divRed, divBlue, divYellow, imagem);
+  section.appendChild(colors);
+
+  buttonReset.addEventListener("click", () => {
+    nextLevel();
+  });
 }
-const blue = document.querySelector('.blue');
-const container = document.querySelector('.container');
-//setInterval(()=>{container = document.querySelector('.container')}, 1000)
-//setInterval(()=>{blue = document.querySelector('.blue')}, 1000)
 
-
-function onOffClass(color){
-    color.c
-    console.log(color)
-    blue.classList.toggle('on');
-    audio(String(blue))//color.className))
-    
+let pc = [green];
+const maxPoints = [];
+let player = [];
+let level = 1;
+let dificulty = 900;
+function generateRandomColor() {
+  let random = Math.floor(Math.random() * 4) + 1;
+  switch (random) {
+    case 1:
+      return red;
+    case 2:
+      return blue;
+    case 3:
+      return yellow;
+    case 4:
+      return green;
+  }
 }
-/* function reciveNumberColor(number){
-    switch(number){
-        case 1:
-            return onOffClass(blue);
-        case 2:
-            return onOffClass(blue);
-        case 3:
-            return onOffClass(blue);
-        case 4:
-            return onOffClass(blue);
-    }
-} */
+
+function changeDificulty() {
+  if (level >= 5 && level < 10) dificulty = 800;
+  if (level >= 10) dificulty = 500;
+}
+
+function nextLevel() {
+  startGame();
+  const levelH3 = document.querySelector("#levelH3");
+  levelH3.innerText = `Nível: ${level}`;
+  maxPoints.push(level);
+  level++;
+}
+
 function audio(color) {
-    let red = {color: "red" , audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3")};
-    let blue = {color: "blue on", audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3")};
-    let yellow = {color: "yellow", audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3")};
-    let green = {color: "green", audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")};
-    if(color === red.color) {
-      red.audio.play();
-    }else if(color === blue.color) {
-      blue.audio.play();
-    }else if(color === green.color) {
-      green.audio.play();
-    }else if(color === yellow.color) {
-      yellow.audio.play();
+  color.audio.play();
+}
+
+function startGame() {
+  changeDificulty();
+  const buttonReset = document.querySelector("#buttonReset");
+  buttonReset.style.display = "none";
+  const pcMessages = document.querySelector("#pcMessages");
+  pcMessages.innerText = `Atenção!`;
+  pcMessages.style.color = "#28a328";
+  setTimeout(() => {
+    pc.forEach((elemen, index, arr) => {
+      setTimeout(() => {
+        audio(arr[index]);
+        toggleColor(arr[index]);
+        if (index === arr.length - 2) {
+          setTimeout(() => (pcMessages.innerText = `Sua vez!`), 400);
+        }
+      }, dificulty * index);
+    });
+    pc.push(generateRandomColor());
+    playerTurn();
+  }, 1000);
+}
+
+function toggleColor(color) {
+  const toggle = document.querySelector(`.${color.color}`);
+  toggle.classList.add("on");
+  setTimeout(() => toggle.classList.remove("on"), 400);
+}
+
+function checkColor(player) {
+  for (let i = 0; i < player.length; i++) {
+    const pcMessages = document.querySelector("#pcMessages");
+    if (player.length === pc.length - 1) {
+      pcMessages.innerText = `Você acertou!`;
+      const buttonReset = document.querySelector("#buttonReset");
+      buttonReset.style.display = "";
+      buttonReset.innerText = "Próximo";
+      buttonReset.removeEventListener("click", nextLevel);
     }
+    if (player[i] !== pc[i]) {
+      pcMessages.style.color = "red";
+      pcMessages.innerText = `Você perdeu!`;
+      reset();
+    }
+  }
 }
 
-container.addEventListener('click', (event) => {
-    const color = event.target.className
-    onOffClass(color);
-    setTimeout(()=> onOffClass(color), 600);
-});
-/* const turn = []
-
-function generateRandom(){
-    let num =  Math.floor(Math.random() * (4) + 1);
-    reciveNumberColor(num)
+function reset() {
+  pc = [green];
+  level = 1;
+  const levelH3 = document.querySelector("#levelH3");
+  const pcMessages = document.querySelector("#pcMessages");
+  levelH3.innerText = `Melhor pontuação: ${Math.max(...maxPoints)}`;
+  const buttonReset = document.querySelector("#buttonReset");
+  buttonReset.style.display = "";
+  buttonReset.innerText = "Jogar Novamente";
 }
 
-const player = document.getElementById('play')
-player.addEventListener('click', generateRandom) */
+function playerTurn() {
+  player = [];
+  const greenButton = document.querySelector(".green");
+  const blueButton = document.querySelector(".blue");
+  const yellowButton = document.querySelector(".yellow");
+  const redButton = document.querySelector(".red");
+  greenButton.addEventListener("click", greenSelected);
+  blueButton.addEventListener("click", blueSelected);
+  yellowButton.addEventListener("click", yellowSelected);
+  redButton.addEventListener("click", redSelected);
+}
 
-
+function greenSelected() {
+  audio(green);
+  toggleColor(green);
+  player.push(green);
+  checkColor(player);
+}
+function blueSelected() {
+  audio(blue);
+  toggleColor(blue);
+  player.push(blue);
+  checkColor(player);
+}
+function yellowSelected() {
+  audio(yellow);
+  toggleColor(yellow);
+  player.push(yellow);
+  checkColor(player);
+}
+function redSelected() {
+  audio(red);
+  toggleColor(red);
+  player.push(red);
+  checkColor(player);
+}
